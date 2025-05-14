6a247f6d79673d4dc3ef2a4c2076889cc1d3b4d7
unknown <quachxuanthinh20092005@gmail.com>
2025-05-14 11:06:36 +0700

    day4

---
+++ Qxtlesoon04/Controllers/QxtBookController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Qxtlesoon04.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Qxtlesoon04.Controllers
+{
+    public class QxtBookController : Controller
+    {
+        private List<QxtBook> qxtBooks;
+
+        public QxtBookController()
+        {
+            Random rand = new Random();
+            qxtBooks = new List<QxtBook>
+            {
+                new QxtBook
+                {
+                    QxtId = "B001",
+                    QxtTitle = "C# Fundamentals",
+                    QxtDescription = "Learn the basics of C# programming language.",
+                    QxtImage = $"B001.jpg",
+                    QxtPrice = 19.99f,
+                    QxtPage = 320
+                },
+                new QxtBook
+                {
+                    QxtId = "B002",
+                    QxtTitle = "ASP.NET Core Guide",
+                    QxtDescription = "A complete guide to building web apps using ASP.NET Core.",
+                    QxtImage = $"image/book-{rand.Next(1, 56)}.jpg",
+                    QxtPrice = 25.50f,
+                    QxtPage = 450
+                },
+                new QxtBook
+                {
+                    QxtId = "B003",
+                    QxtTitle = "Entity Framework Deep Dive",
+                    QxtDescription = "Explore Entity Framework in depth for data access.",
+                    QxtImage = $"image/book-{rand.Next(1, 56)}.jpg",
+                    QxtPrice = 22.00f,
+                    QxtPage = 380
+                },
+                new QxtBook
+                {
+                    QxtId = "B004",
+                    QxtTitle = "LINQ in Action",
+                    QxtDescription = "Master Language Integrated Query with real-world examples.",
+                    QxtImage = $"image/book-{rand.Next(1, 56)}.jpg",
+                    QxtPrice = 17.75f,
+                    QxtPage = 290
+                },
+                new QxtBook
+                {
+                    QxtId = "B005",
+                    QxtTitle = "Blazor for Beginners",
+                    QxtDescription = "Get started with Blazor and build interactive web UIs.",
+                    QxtImage = $"image/book-{rand.Next(1, 56)}.jpg",
+                    QxtPrice = 21.90f,
+                    QxtPage = 310
+                }
+            };
+        }
+
+        // GET: /QxtBook/QxtIndex
+        public IActionResult QxtIndex()
+        {
+            return View(qxtBooks);
+        }
+        public IActionResult QxtCreate()
+        {
+           QxtBook qxtBook = new QxtBook();
+            return View(qxtBook);
+        }
+    }
+}
---
+++ Qxtlesoon04/Models/QxtBook.cs
@@ -0,0 +1,13 @@
+﻿namespace Qxtlesoon04.Models
+{
+    public class QxtBook
+    {
+        public string QxtId { get; set; }
+        public string QxtTitle { get; set; }
+        public string QxtDescription { get; set; }
+        public string QxtImage { get; set; }
+        public float QxtPrice { get; set; }
+        public int QxtPage { get; set; }
+
+    }
+}
