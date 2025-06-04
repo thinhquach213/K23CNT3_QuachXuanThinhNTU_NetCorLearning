2831475eb5ce7755b87aeb8aae3af21a75da46b4
unknown <quachxuanthinh20092005@gmail.com>
2025-06-04 10:49:57 +0700

    day07

---
+++ QxtLesson07/Models/QxtEmployee.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QxtLesson07.Models
+{
+    public class QxtEmployee
+    {
+        
+        public int QxtID { get; set; }
+
+        
+        public string QxtName { get; set; }
+
+       
+        public DateTime QxtBirthDay { get; set; }
+
+        
+        public string QxtEmail { get; set; }
+
+      
+        public string QxtPhone { get; set; }
+
+       
+        public decimal QxtSalary { get; set; }
+
+        public bool QxtStatus { get; set; }
+    }
+
+}
